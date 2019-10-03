ce759455e6c521e4223ad9408591b07ce78ad8bd
karimhaddad <karim.haddad@ircam.fr>
2019-10-03 21:40:24 +0200

    Cleaned sources

--- omlily 3.1/sources/omlily-gen.lisp
+++ omlily 3.1/sources/omlily-gen.lisp
@@ -482,28 +482,6 @@ remaining cent value (i.e., midi-cent 8176 would be expressed as Bb4-24)."
 
 
 
-
-
-#|
-;;already defined above
-(setf *voice-rank*
-      '((1 . "one") (2 . "two")(3 . "three") (4 . "four")
-      (5 . "five") (6 . "six")(7 . "seven") (8 . "eight")
-      (9 . "nine") (10 . "ten")(11 . "eleven") (12 . "twelve")
-      (13 . "thirteen") (14 . "fourteen")(15 . "fifteen") (16 . "sixteen")
-      (17 . "seventeen") (18 . "eighteen")(19 . "nineteen") (20 . "twenty")
-      (21 . "twentyone") (22 . "twentytwo")(23 . "twentythree") (24 . "twentyfour")
-      (25 . "twentyfive") (26 . "twentysix")(27 . "twentyseven") (28 . "twentyeight")
-      (29 . "twentynine") (30 . "thirty")(31 . "thirtyone") (32 . "thirtytwo")
-      (33 . "thirtythree") (34 . "thirtyfour")(35 . "thirtyfive") (36 . "thirtysix")))
-|#
-
-;(cassq 30 *voice-rank*)
-
-
-
-
-
 ;(defun massq (item list)
 ;(format nil "~S" (cdr (assoc item list :test 'equal))))  
 
